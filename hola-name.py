4b16cbfb271fe24b1d01487bbad42371e99ab956
Alvaro416 <Aforosalvaro@gmail.com>
2021-01-28 20:23:03 +0100

    primer cambio

---
+++ hola-name.py
@@ -0,0 +1,10 @@
+
+""" 
+    Escribir script que pregunte el nombre del usuario en la consola y el numero entero e imprima 
+    por pantalla en lineas distintas el nombre del usuario tantas veces como el numero que le hayas indicado.5656
+
+"""
+
+name = input("¿Como te llamas? ")
+n = input("Introduce un 6dsfhnúmero entero: ")
+print ((name + "\n") * int(n))
