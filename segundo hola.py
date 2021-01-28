dd5c8b0c031b57405b49ae8cb81cb3723400c1ef
Alvaro416 <Aforosalvaro@gmail.com>
2021-01-28 21:08:14 +0100

    guardado

---
+++ segundo hola.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""Escribir un programa que pregnte el nombre del usuario en la consola y despues de que el 
+    usuario por pantalla la cadena  "Hola Nombre ", donde nombre, es el nombre que el usuario 
+    haya elegido
+"""
+
+name = input("Introduce tu nombre: ")
+print("¡Hola" + name + "!")
